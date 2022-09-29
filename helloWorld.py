4236e1c2abb511020a3b38c54111cb0a98a6ba2e
Raju Thapa <rajuthap06@gmail.com>
2022-09-28 23:54:52 -0400

    task1

--- helloWorld.py
+++ helloWorld.py
@@ -2,12 +2,3 @@
 greeting = "Hello, World!"
 print(greeting)
 
-#task 3
-my_disc = {"Make":"Tesla", "Model": "Y"}
-print("select - Make or Model")
-index = input("Make a selection from above list: ")
-if (index == "make"):    
-    text = my_disc["Make"]  
-else:
-    text = my_disc["Model"]    
-print(text)
