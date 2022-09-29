feed4365da5207b9232b4f369be57a459eef666f
Raju Thapa <rajuthap06@gmail.com>
2022-09-28 22:57:56 -0400

    task 3

--- helloWorld.py
+++ helloWorld.py
@@ -7,3 +7,13 @@ my_list = ["apple", "orange", "mango", "banana"]
 index = input("Input from 1-4 and see fruits selected: ")
 index = int(index) - 1
 print(my_list[index])
+
+#task 3
+my_disc = {"Make":"Tesla", "Model": "Y"}
+print("select - Make or Model")
+index = input("Make a selection from above list: ")
+if (index == "make"):    
+    text = my_disc["Make"]  
+else:
+    text = my_disc["Model"]    
+print(text)
