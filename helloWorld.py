71ba4784add82a92415ca1e0dc978bfaa32cc6ad
Raju Thapa <rajuthap06@gmail.com>
2022-09-28 23:48:44 -0400

    task 1

--- helloWorld.py
+++ helloWorld.py
@@ -2,12 +2,6 @@
 greeting = "Hello, World!"
 print(greeting)
 
-# task 2
-my_list = ["apple", "orange", "mango", "banana"]
-index = input("Input from 1-4 and see fruits selected: ")
-index = int(index) - 1
-print(my_list[index])
-
 #task 3
 my_disc = {"Make":"Tesla", "Model": "Y"}
 print("select - Make or Model")
